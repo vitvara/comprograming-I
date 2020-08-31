eb1ef8d7e5c9e159a3a6a87c12b812cdc94536a3
vitvara <vitvara1@gmail.com>
2020-08-31 20:07:27 +0700

    add pep8

--- elap/test.py
+++ elap/test.py
@@ -1,20 +1,23 @@
-# ++ -+ -- +-
-print("Input a point (x,y):")
-x = float(input("x = ? "))
-y = float(input("y = ? "))
+check = 0
+size_1 = float(input("Enter 1st line's length: "))
+size_2 =  float(input("Enter 2nd line's length: "))
+size_3 = float(input("Enter 3rd line's length: "))
 
-if x > 0 and y > 0:
-    print(f"The point ({x:.2f},{y:.2f}) is in quadrant 1.")
-elif x < 0 and y > 0:
-    print(f"The point ({x:.2f},{y:.2f}) is in quadrant 2.")
-elif x < 0 and y < 0:
-    print(f"The point ({x:.2f},{y:.2f}) is in quadrant 3.")
-elif x > 0 and y < 0:
-    print(f"The point ({x:.2f},{y:.2f}) is in quadrant 4.")
-elif x == 0 and y == 0:
-    print("The point (0.00,0.00) is at the origin.")
-elif x == 0:
-    print(f"The point (0.00,{y:2.f}) is on the y-axis.")
-elif y == 0:
-    print(f"The point ({x:.2f},0.00) is on the x-axis.")
+if size_1**2 == size_3**2 + size_2 **2:
+    print("It's a right triangle.")
+elif size_2**2 == size_1**2 + size_3 **2:
+    print("It's a right triangle.")
+elif size_3**2 == size_1**2 + size_2 **2:
+    print("It's a right triangle.")
 
+elif size_1 + size_2 < size_3:
+    print("It's not a triangle.")
+                                                                                                                                         
+elif size_1 + size_3 < size_2:
+    print("It's not a triangle.")
+       
+elif size_3 + size_2 < size_1:
+    print("It's not a triangle.")
+else:
+    print("It's a triangle but not a right triangle.")
+    
