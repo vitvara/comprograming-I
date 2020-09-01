82326b0f6a3bc4930fa6aaad828736eec3a851cc
vitvara <vitvara1@gmail.com>
2020-09-01 23:43:54 +0700

    check square_root.py and clear test.py

--- elap/test.py
+++ elap/test.py
@@ -1,4 +1,18 @@
-def change_second_char(s, c):
-    return s[0] + c + s[2:]
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+import math
+def my_sqrt(a):
+    x = int(input("Enter num: "))
+    while True:
+        y = (x + a/x) / 2
+        if y == x:
+         break
+        x = y
 
-print(change_second_char("sssss","d"))
+def test_square_root():
+    print(f"a  mysqrt(a)  math.sqrt(a)  diff")
+    print(f"-  ---------  ------------  ----")
+    for i in a:
+        print(f"{i}   {round(my_root(i), 11):<14}{round(math.sqrt(i), 11):<14}{math.sqrt(i) - my_root(i)}")
+
+print(test_square_root())
+
